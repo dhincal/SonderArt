99da10e46576d87692a6366e1978cc945489be07
Deniz Hınçal <dhinc@uic.edu>
2025-02-08 05:44:59 -0600

    REmove REquest

--- server/app/api/events/route.ts
+++ server/app/api/events/route.ts
@@ -1,8 +1,7 @@
 import db from "@/db";
 import { eventTable } from "@/db/schema";
 
-export async function GET(request: Request) {
-  console.log(request);
+export async function GET() {
   const events = await db.select().from(eventTable);
   console.log(events.length);
   return new Response(JSON.stringify(events), {
